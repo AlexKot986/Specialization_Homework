1cf344df89b8f91457a8b18286e035a4ac823023
Alexershov86 <alexershov86@mail.ru>
2023-02-24 22:15:17 +0300

    добавил метод SortStringArray() и описание в README

--- homework/Program.cs
+++ homework/Program.cs
@@ -2,3 +2,14 @@
 string[] test2 = {"1234", "1567", "-2", "computer science"};
 string[] test3 = {"Russia", "Denmark", "Kazan"};
 
+string[] SortStringArray(string[] test, int n)
+{
+    string result = string.Empty;
+
+    for (int i = 0; i < test.Length; i ++)
+        if (test[i].Length <= n) result += "," + test[i] ;
+
+    if (result == string.Empty) return new string[0];
+
+    return result.Substring(1).Split(',');
+}
